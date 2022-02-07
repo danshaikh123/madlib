2b94e2a0f116bdfe311aee2800b71f1a8a16cfad
danshaikh123 <75819204+danshaikh123@users.noreply.github.com>
2022-02-07 16:49:57 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,14 @@
+
+# Initializing the input variables that will be used in the f-string, madlib.
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Verb 2: ")
+celebrity = input("Celebrity: ")
+
+# Initializing the f-string variable, madlib
+madlib = f"Computer Programming is so {adj}! It make me so excited all the time because \
+I love to {verb}. Stay hydrated and {verb2} like your are {celebrity}."
+
+# Printing madlib
+print(madlib)
+
